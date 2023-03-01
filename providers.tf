4116bed59b63bcaf884c48e4133ae62679a0124e
“MounikaMalineni” <mounikamalineni1@gmail.com>
2023-03-01 13:57:13 +0000

    Added gitignore

---
+++ providers.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscode"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+
+   backend "s3" {
+    key            = "terraform.tfstate"
+    bucket         = "my-tf-sf-bucket"
+    region         = "eu-west-2"
+    dynamodb_table = "tf-lock-table"
+    profile = "vscode"
+  }
+}
